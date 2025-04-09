fd5c1732aa4ad121d73582e5a67690435037d7c4
tearzday <biverdaniil@gmail.com>
2025-04-09 13:32:35 +0300

    feat: add memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -115,8 +115,18 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cash;
+
+  function startFunc() {
+    if (!cash) {
+      cash = func();
+    }
+
+    return cash;
+  }
+
+  return startFunc;
 }
 
 /**
